53557bd76e787396b06dade17d60792a15cfddce
Robert Tunney <Robert.Tunney@gmail.com>
2012-12-19 13:57:53 -0500

    fixed allowed single/double/triple angles of atoms upon adding singles.

--- static/chem_draw.js
+++ static/chem_draw.js
@@ -2,6 +2,12 @@ var color = {'H':'#aaaaaa', 'C':'#222222', 'N':'#2233ff', 'O':'#ff2200', 'F':'#5
 var valence = {'H':1, 'He':0, 'Li':1, 'Be':2, 'B':3, 'C':4, 'N':3, 'O':2, 'F':1, 'Ne':0, 'Na':1, 'Mg':2, 'Al':3, 'Si':4, 'P':5, 'S':6, 'Cl':5, 'Ar':0, 'K':1, 'Ca':2, 'Sc':3, 'Ti':4, 'V':5, 'Cr':6, 'Mn':4, 'Fe':4, 'Co':4, 'Ni':4, 'Cu':2, 'Zn':2, 'Ga':3, 'Ge':4, 'As':5, 'Se':6, 'Br':5, 'Kr':2, 'Rb':1, 'Sr':2, 'Y':3, 'Zr':4, 'Nb':5, 'Mo':6, 'Tc':6, 'Ru':6, 'Rh':6, 'Pd':4, 'Ag':3, 'Cd':2, 'In':3, 'Sn':4, 'Sb':5, 'Te':6, 'I':7, 'Xe':6, 'Cs':1, 'Ba':2, 'La':3, 'Ce':4, 'Pr':4, 'Nd':3, 'Pm':3, 'Sm':3, 'Eu':3, 'Gd':3, 'Tb':4, 'Dy':3, 'Ho':3, 'Er':3, 'Tm':3, 'Yb':3, 'Lu':3, 'Hf':4, 'Ta':5, 'W':6, 'Re':7, 'Os':6, 'Ir':6, 'Pt':6, 'Au':5, 'Hg':4, 'Tl':3, 'Pb':4, 'Bi':5, 'Po':6, 'At':7, 'Rn':6, 'Fr':1, 'Ra':2, 'Ac':3, 'Th':4, 'Pa':5, 'U':6, 'Np':6, 'Pu':6, 'Am':4, 'Cm':4, 'Bk':4, 'Cf':4, 'Es':4, 'Fm':3, 'Md':3, 'No':3, 'Lr':3, 'Rf':4, 'Db':5, 'Sg':6, 'Bh':7, 'Hs':7, 'Mt':7, 'Ds':7, 'Rg':7, 'Cn':6, 'Uut':5, 'Uuq':4}
 var atoms = [];
 
+var remove = function(val, list){
+    var index = list.indexOf(val);
+    list.splice(index, 1);
+    return list.slice(0, index).concat(list.slice(index+1, list.length));
+}
+
 var toDeg = function(x){
     return x*180/Math.PI;
 }
@@ -14,7 +20,7 @@ var findAtom = function(x, y) {
     for (var i=0; i<atoms.length; i++) {
         var atom = atoms[i];
         if (Math.sqrt(Math.pow(x-atom.x, 2) + Math.pow(y-atom.y, 2))<10) {
-            console.log(atom);
+            // console.log(atom);
             return atom;
         }
     }
@@ -66,7 +72,7 @@ var atomData = function(mode, baseAng, x, y, g){
             this.element = '[enter]';
             this.maxValence = 4;
             this.curValence = 0;
-            this.baseAng = 0;
+            this.baseAng = 30;
             this.singleAngs = [30, 150, 270];
             this.doubleAngs = [30, 150, 270];
             this.tripleAngs = [0, 180];
@@ -81,7 +87,7 @@ var atomData = function(mode, baseAng, x, y, g){
             this.element = 'C';
             this.maxValence = 4;
             this.curValence = 1;
-            this.baseAng = (baseAng+180)%360;
+            this.baseAng = baseAng;
             this.singleAngs = [(this.baseAng+120)%360, (this.baseAng+240)%360];
             this.doubleAngs = [(this.baseAng+120)%360, (this.baseAng+240)%360];
             this.tripleAngs = [(this.baseAng+180)%360];
@@ -98,7 +104,7 @@ var atomData = function(mode, baseAng, x, y, g){
             this.element = 'C';
             this.maxValence = 4;
             this.curValence = 2;
-            this.baseAng = (baseAng+180)%360;
+            this.baseAng = baseAng;
             this.singleAngs = [(this.baseAng+120)%360, (this.baseAng+240)%360];
             this.doubleAngs = [(this.baseAng+180)%360];
             this.tripleAngs = [];
@@ -113,7 +119,7 @@ var atomData = function(mode, baseAng, x, y, g){
             this.element = 'C';
             this.maxValence = 4;
             this.curValence = 3;
-            this.baseAng = (baseAng+180)%360;
+            this.baseAng = baseAng;
             this.singleAngs = [(this.baseAng+180)%360];
             this.doubleAngs = [];
             this.tripleAngs = [];
@@ -154,18 +160,18 @@ elementMaker.atom = function(x, y, baseAng){
     else if (sourceAtom) { var baseAng = atom.baseAng; }
     else { var baseAng = 0; }
 
-    var atom = new atomData(mode, baseAng, x, y-5, current[0][0]); 
+    var atom = new atomData(mode, (baseAng+180)%360, x, y-5, current[0][0]); 
     // var atom = {x+(text.getBBox().width/3), y:y-5, element:null, maxValence:null, curValence:0, baseAng:0, single_angles:[], double_angles:[60, -60], triple_angles:[0], singles:0, g:current[0][0], text:text, removeText:function(){text.textContent='';}};
     atoms.push(atom);
 
     var clicked = function(e){
-        console.log('atom clicked');
+        // console.log('atom clicked');
         if (mode=='delete') {
             document.getElementById('whiteboard').removeChild(current[0][0]);
             atoms.splice(atoms.indexOf(atom), 1);
         }
         else {
-            console.log('request to change element text');
+            // console.log('request to change element text');
             var oldtxt = current[0][0].firstChild.textContent;
             var txtinput = document.createElement('input');
             txtinput.setAttribute('type', 'text');
@@ -205,6 +211,8 @@ elementMaker.atom = function(x, y, baseAng){
     current.attr('id', null);
     if (mode=='atom') {clicked(); }
     else {atom.element = 'C'; atom.x = x+text.getBBox().width/2; atom.maxValence = valence['C'];}
+
+    return atom
 }
 
 elementMaker.benzene = function(x, y){
@@ -247,10 +255,11 @@ elementMaker.benzene = function(x, y){
 elementMaker.single = function(x, y) {
     var atom = findAtom(x, y);
     if (atom) { 
-        console.log("x: " + x);
-        console.log("y: " + y);
-        console.log("atom.x: " + atom.x);
-        console.log("atom.y: " + atom.y);
+        if (atom.curValence>=atom.maxValence) { return "max valence exceeded"; }
+    //     console.log("x: " + x);
+    //     console.log("y: " + y);
+    //     console.log("atom.x: " + atom.x);
+    //     console.log("atom.y: " + atom.y);
         var dx = x-atom.x;
         var dy = atom.y-y;
         var x = atom.x; 
@@ -258,15 +267,21 @@ elementMaker.single = function(x, y) {
         if (atom.element=='C') {
             atom.text.style.fill = '#fff';
         }
+        atom.curValence += 1;
         atom.singles+=1;
     }
 
-    console.log(dx);
-    console.log(dy);
-    console.log(atom.singleAngs);
-    console.log(findAngle(dx, dy, atom.singleAngs));
+    // console.log(dx);
+    // console.log(dy);
+    // console.log(atom.singleAngs);
+    // console.log(findAngle(dx, dy, atom.singleAngs));
 
     var rotate = findAngle(dx, dy, atom.singleAngs);
+
+    remove(rotate, atom.singleAngs);
+    remove(rotate, atom.doubleAngs);
+    if (atom.curValence===1) {atom.tripleAngs = [(rotate+180)%360];}
+    else { atom.tripleAngs = []; }
 
     d3.select('#whiteboard')
     .append('g')
@@ -284,8 +299,24 @@ elementMaker.single = function(x, y) {
     .attr('style', "stroke:#333; stroke-width:3;");
 
     current.attr('id', null);
-    
-    elementMaker.atom(x + Math.round(40*Math.cos(toRad(rotate))), y - Math.round(40*Math.sin(toRad(rotate))) - 2, rotate);
+
+    var x2 = x+Math.round(40*Math.cos(toRad(rotate)));
+    var y2 = y - Math.round(40*Math.sin(toRad(rotate))) - 2;
+
+    var secondAtom = findAtom(x2, y2)
+
+    if (secondAtom) { 
+        // console.log('second atom found!!!');
+        secondAtom.singles += 1; 
+        secondAtom.curValence += 1; 
+        remove((rotate+180)%360, secondAtom.singleAngs);
+        remove((rotate+180)%360, secondAtom.doubleAngs);
+        secondAtom.tripleAngs = [];
+    }
+    else {
+        elementMaker.atom(x2, y2, rotate);
+        var secondAtom = findAtom(x2, y2);
+    }
 }
 
 elementMaker.double = function(x, y) {
